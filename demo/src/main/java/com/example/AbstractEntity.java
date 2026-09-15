08a6b03839e0644297e0342981e1f5848e79f7a5
Playly <sarr74824@gmail.com>
2026-09-15 13:36:56 +0000

    feat: ajouter les salles de demonstration

---
+++ demo/src/main/java/com/example/AbstractEntity.java
@@ -0,0 +1,5 @@
+package main.java.com.example;
+
+public class AbstractEntity {
+    public string id;
+}
---
+++ demo/src/main/java/com/example/Salle.java
@@ -0,0 +1,8 @@
+package main.java.com.example;
+
+public class Salle extends AbstractEntity{
+    public Salle(String id){
+        super(id);
+    }
+   
+}
