4145e7bd5dba890c8c7efaeb99e1f7e04dc77158
Playly <sarr74824@gmail.com>
2026-09-15 13:38:44 +0000

    docs: documenter le modele objet

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -1,13 +1,17 @@
 package com.example;
-
+import  main.java.com.example.Salle;
+import main.java.com.example.Reservation;
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ){
+    Salle salle1=new Salle("1");
+    Salle salle1=new Salle("2");
+    Salle salle1=new Salle("3");
+    Salle salle1=new Salle("4");
+    Salle salle1=new Salle("5");
     }
 }
